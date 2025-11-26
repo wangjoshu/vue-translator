370419ba96ad75d60db65affd8f91587ae170b7e
wangjoshu <15390257620@163.com>
2025-11-26 11:49:49 +0800

    修改前端调用后端

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://vue-translator-production.up.railway.app/', // 改为你的Railway域名
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+  define: {
+    // 注入环境变量
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://你的项目名.up.railway.app')
+  }
 })
 
