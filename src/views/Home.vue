c940c8227031766cd4aa12b42710204af52ba57a
wangjoshu <15390257620@163.com>
2025-11-27 13:58:13 +0800

    屏幕大小

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,6 +17,8 @@ import TranslationForm from '@/components/TranslationForm.vue';
 @media screen and (max-width: 768px) {
     * {
         width: 100% !important;
+        margin: 0 auto;
+        overflow-x: hidden;
     }
 }
 </style>
